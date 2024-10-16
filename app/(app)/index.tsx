ab509dfe2b6c4bfc372f1fbebf9a2784329b72fa
Błażej Tondel <badziej12@gmail.com>
2024-10-17 00:33:45 +0200

    Add header ad post view
    Changes made:
     - Implemented header for Home screen
     - Implemented Post view
     - Still playground

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,6 +1,9 @@
 import { View, Text, Button, Pressable } from 'react-native';
 import { useAuth } from '@/context/authContext';
-import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { Stack } from 'expo-router';
+import HomeHeader from '@/components/HomeHeader';
+import { Post } from '@/components/Post';
 
 export default function Home() {
   const { logout, user } = useAuth();
@@ -10,12 +13,26 @@ export default function Home() {
 
   console.log("To user: ", user);
   return (
-    <View style={{paddingTop: 40}} className="bg-red-200">
-      <Text>Home i cos</Text>
-      <Text>Siema {user?.username}</Text>
-      <Pressable onPress={handleSignOut} style={{height: hp(6.5)}} className="bg-indigo-500 rounded-xl justify-center">
-        <Text className="text-white font-bold tracking-wider text-center">Sign Out</Text>
-      </Pressable>
+    <View style={{paddingTop: 40}} className="flex-1 bg-white px-5">
+      <Stack.Screen
+        options={{
+          header: () => <HomeHeader />
+        }}
+      />
+      <View className="flex-1">
+        <View className="mb-5">
+          <Text className="font-semibold" style={{fontSize: hp(3)}}>Witaj {user?.username}!</Text>
+        </View>
+        <Post postTitle="Tytuł posta" postCopy="Jakiś tekst" author="Autor" />
+      </View>
+      <View className="px-5 flex-row gap-2.5" style={{marginBottom: hp(5)}}>
+        <Pressable onPress={handleSignOut} style={{height: hp(6.5)}} className="bg-indigo-500 flex-grow rounded-xl justify-center">
+          <Text className="text-white font-bold tracking-wider text-center">Sign Out</Text>
+        </Pressable>
+        <Pressable onPress={handleSignOut} style={{height: hp(6.5)}} className="bg-indigo-300 flex-grow rounded-xl justify-center">
+          <Text className="text-white font-bold tracking-wider text-center">Add new</Text>
+        </Pressable>
+      </View>
     </View>
   )
 }

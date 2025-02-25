5cabd8e86c5012146b4f03ff67f8e5ce16de1349
badziej12 <badziej12@gmail.com>
2025-02-26 00:53:51 +0100

    Implement and display groups
    Changes made:
     - Introduced GroupCard component which display group info in groups tab
     - Main groups screen now fetching groups data for specific user

--- app/(app)/(tabs)/(groups)/index.tsx
+++ app/(app)/(tabs)/(groups)/index.tsx
@@ -1,12 +1,66 @@
-import { Pressable, View, Text } from "react-native"
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import { router } from "expo-router";
+import { Pressable, View, Text, ScrollView } from "react-native"
+import { heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { router, useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
+import { GroupCard } from "@/components/GroupCard";
+import { useAuth } from "@/context/authContext";
+import { doc, getDoc } from "firebase/firestore";
+import { groupsRef, usersRef } from "@/firebaseConfig";
 
 export default function Groups() {
+    const [groups, setGroups] = useState<{ name: string; members: string[]; groupSize: number }[]>([]);
+
+    const { user } = useAuth();
+    
+    const fetchUserGroups = async () => {
+        if (user?.userId) {
+            const userRef = doc(usersRef, user?.userId);
+            const docSnap = await getDoc(userRef);
+
+            if (docSnap.exists()) {
+                const userData = docSnap.data();
+                const userGroups = userData.groups || [];
+
+                const groupDetails = await Promise.all(userGroups.map(async (groupId: string) => {
+                    const groupRef = doc(groupsRef, groupId);
+                    const groupSnap = await getDoc(groupRef);
+
+                    const groupName = groupSnap.get("name");
+                    const groupSize = groupSnap.get("members").length;
+                    const members = await Promise.all(groupSnap.get("members").map(async (memberId: string) => {
+                        const memberRef = doc(usersRef, memberId);
+                        const memberSnap = await getDoc(memberRef);
+
+                        if (docSnap.exists()) {
+                            return memberSnap.get("username");
+                        }
+                    }));
+        
+                    return groupSnap.exists() ? { name: groupName, groupSize: groupSize, members: members } : null;
+                }));
+
+                console.log(groupDetails);
+                setGroups(groupDetails);
+            }
+        }
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchUserGroups();
+        }, [user])
+    );
 
     return (
-        <View style={{paddingTop: 40}} className="flex-1 bg-white px-5">
+        <View className="flex-1 bg-white px-5">
             <View className="flex-1">
+                <ScrollView style={{paddingTop: 40}} className="flex-1 px-5">
+                    <View className="flex-col gap-5">
+                        {groups.map(group => 
+                            <GroupCard key={group.name} name={group.name} members={group.members} groupSize={group.groupSize} />
+                        )}
+                    </View>
+                </ScrollView>
                 <View className="px-5 flex-row gap-2.5" style={{marginBottom: hp(5)}}>
                     <Pressable onPress={() => router.push("/createGroup")} style={{height: hp(6.5)}} className="bg-indigo-500 flex-grow rounded-xl justify-center">
                         <Text className="text-white font-bold tracking-wider text-center">Create</Text>

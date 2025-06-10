ad24c6d808a772cd6a8783ccbe1c9321f346d737
badziej12 <badziej12@gmail.com>
2025-06-10 19:50:47 +0200

    Fix exercise selection in ExerciseModal
    Changes made:
     - ExerciseModal component is now wrapped with Modal component outside
     - Exerises in ExerciseModal are now unselected after adding them

--- components/Screens/addTraining/ExerciseModal.tsx
+++ components/Screens/addTraining/ExerciseModal.tsx
@@ -10,8 +10,6 @@ import { useAppDispatch, useAppSelector } from "@/store/store";
 import { exerciseActions } from "@/store/exercise/exercise-slice";
 
 type ExerciseModalProps = {
-    isModalVisible: boolean,
-    onCloseModal: () => void,
     onAddExercise: (exerciseName: string[]) => void,
 }
 
@@ -20,7 +18,7 @@ export type AvailableExerciseType = {
     category: string[];
 }
 
-const ExerciseModal: FC<ExerciseModalProps> = ({ isModalVisible, onCloseModal, onAddExercise }) => {
+const ExerciseModal: FC<ExerciseModalProps> = ({ onAddExercise }) => {
     const [searchText, setSearchText] = useState("");
     const [selectedExercises, setSelectedExercises] = useState<string[]>([]);
     const availableExercises = useAppSelector(state => state.exercise.availableExercises);
@@ -51,55 +49,43 @@ const ExerciseModal: FC<ExerciseModalProps> = ({ isModalVisible, onCloseModal, o
         });
     }
 
-    const fetchAvailableExercises = async () => {
-        const docSnap = await getDocs(trainingsRef);
-        const data = docSnap.docs.map(training => training.data()) as AvailableExerciseType[];
-        dispatch(exerciseActions.setAvailableExercises(data));
-    }
-
     useEffect(() => {
+        const fetchAvailableExercises = async () => {
+            const docSnap = await getDocs(trainingsRef);
+            const data = docSnap.docs.map(training => training.data()) as AvailableExerciseType[];
+            dispatch(exerciseActions.setAvailableExercises(data));
+        }
+
         fetchAvailableExercises();
     }, [])
 
     return (
-        <Modal
-            animationType="fade"
-            transparent={true}
-            className="bg-transparent gowno p-12"
-            visible={isModalVisible}
-            onRequestClose={() => {
-                onCloseModal();
-            }}
-        >
-            <View className="px-2 py-8 flex-1" style={{ backgroundColor: "#000000BF" }}>
-                <SafeAreaProvider>
-                    <SafeAreaView className="flex-1">
-                        <View className="p-4 h-full" style={{ backgroundColor: "#414141", padding: 16 }}>
-                            <View className="flex-row justify-between mb-4">
-                                <Text style={styles.heading} className="uppercase text-white">Add new exercise</Text>
-                                <Pressable onPress={() => onAddExercise(selectedExercises)} className=""><Text className="uppercase text-sunny" style={styles.heading}>Add+</Text></Pressable>
-                            </View>
-                            <SearchComponent searchText={searchText} onSearchChange={handleSearchChange} />
-                            <ScrollView >
-                                {Object.keys(groupedAvailableExercises).map(letter => (
-                                    <View key={letter} className="mb-4">
-                                        <View className="flex-row items-center mb-4">
-                                            <Text style={styles.letterHeading}>{letter}</Text>
-                                            <View className="h-0.5 w-full bg-white" />
-                                        </View>
-                                        {groupedAvailableExercises[letter].map(exercise => (
-                                            <ExerciseCard key={exercise.name} exerciseName={exercise.name} exerciseCategory={exercise.category} selectedExercises={selectedExercises} onSelectedExercise={handleSelectExercise} />
-                                        ))}
-                                    </View>
-                                ))}
-                            </ScrollView>
+        <View className="px-2 py-8 flex-1" style={{ backgroundColor: "#000000BF" }}>
+            <SafeAreaProvider>
+                <SafeAreaView className="flex-1">
+                    <View className="p-4 h-full" style={{ backgroundColor: "#414141", padding: 16 }}>
+                        <View className="flex-row justify-between mb-4">
+                            <Text style={styles.heading} className="uppercase text-white">Add new exercise</Text>
+                            <Pressable onPress={() => onAddExercise(selectedExercises)} className=""><Text className="uppercase text-sunny" style={styles.heading}>Add+</Text></Pressable>
                         </View>
-                    </SafeAreaView>
-                </SafeAreaProvider>
-            </View>
-        </Modal>
-
-
+                        <SearchComponent searchText={searchText} onSearchChange={handleSearchChange} />
+                        <ScrollView >
+                            {Object.keys(groupedAvailableExercises).map(letter => (
+                                <View key={letter} className="mb-4">
+                                    <View className="flex-row items-center mb-4">
+                                        <Text style={styles.letterHeading}>{letter}</Text>
+                                        <View className="h-0.5 w-full bg-white" />
+                                    </View>
+                                    {groupedAvailableExercises[letter].map(exercise => (
+                                        <ExerciseCard key={exercise.name} exerciseName={exercise.name} exerciseCategory={exercise.category} selectedExercises={selectedExercises} onSelectedExercise={handleSelectExercise} />
+                                    ))}
+                                </View>
+                            ))}
+                        </ScrollView>
+                    </View>
+                </SafeAreaView>
+            </SafeAreaProvider>
+        </View>
     )
 }
 
